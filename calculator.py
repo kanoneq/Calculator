785865b41f80604053ae114338c7fbab07b2f381
kanoneq <kanoneqisgone@gmail.com>
2024-04-19 16:37:40 +0200

    this thing is hard

--- calculator.py
+++ calculator.py
@@ -1,3 +1,20 @@
 import tkinter as tk
 from tkinter import messagebox
 
+root = tk.Tk()
+
+root.title("Calculator (by kanoneq :D)")
+root.geometry("600x750")
+
+label = tk.Label(root, text = "Welcome to my calculator!", font=('Arial', 18))
+label.pack()
+
+buttonframe = tk.Frame(root)
+buttonframe.columnconfigure(0, weight=1)
+buttonframe.columnconfigure(1, weight=1)
+buttonframe.columnconfigure(2, weight=1)
+buttonframe.columnconfigure(3, weight=1)
+
+
+
+root.mainloop()
