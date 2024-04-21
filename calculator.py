4c89ebb629feea403123203380a1b17753e5aafc
kanoneq <kanoneqisgone@gmail.com>
2024-04-21 13:37:18 +0200

    "C" button and "backspace" button added, and some minor changes in previous code

--- calculator.py
+++ calculator.py
@@ -4,18 +4,39 @@ from tkinter import messagebox
 root = tk.Tk()
 
 root.title("Calculator (by kanoneq :D)")
-root.geometry("500x300")
+root.geometry("500x400")
 
-label = tk.Label(root, text = "Welcome to my calculator!", font=('Arial', 18))
-label.pack()
+#Cframe (thingy for backspace and "C" button)
+Cframe = tk.Frame(root)
+Cframe.columnconfigure(0, weight=1)
+Cframe.columnconfigure(1, weight=2)
 
+def clear():
+    empty: str = ""
+    Label.configure(text=empty)
+btnC = tk.Button(Cframe, text="C", font=('Arial', 20), command=clear)
+btnC.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+def back():
+    new = Label["text"]
+    new = new[:len(new)-1]
+    Label.configure(text=new)
+btnback = tk.Button(Cframe, text="backspace", font=('Arial', 20), command=back)
+btnback.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+Cframe.pack(fill="x")
+#end of Cframe
+
+#label which shows what user is typing into calculator
 Label = tk.Label(root, text="", font=('Arial', 20))
 Label.pack(padx=5, pady=20)
 def adding(string: str):
     current = Label["text"]
     new = current + string
     Label.configure(text=new)
+#end of the label
 
+#buttonframe is basically every button on the bottom of the calculator
 buttonframe = tk.Frame(root)
 buttonframe.columnconfigure(0, weight=1)
 buttonframe.columnconfigure(1, weight=1)
@@ -62,19 +83,6 @@ def zero():
     adding("0")
 btn0 = tk.Button(buttonframe, text="0", font=("Arial", 18), command=zero)
 btn0.grid(row=3, column=1, sticky=tk.W+tk.E)
-def eq():
-    current = Label["text"]
-    
-    result: str = ""
-    try:
-        result = eval(current)
-    except Exception as e:
-        result = "ERROR! Check console."
-        print(f"error: {e}")
-
-    Label.configure(text=result)
-btn_eq = tk.Button(buttonframe, text="=", font=("Arial", 18), command=eq)
-btn_eq.grid(row=3, column=2, sticky=tk.W+tk.E)
 def add2():
     adding("+")
 btn_add = tk.Button(buttonframe, text="+", font=("Arial", 18), command=add2)
@@ -96,31 +104,22 @@ def dot():
 btn_dot = tk.Button(buttonframe, text=".", font=("Arial", 18), command=dot)
 btn_dot.grid(row=3, column=0, sticky=tk.W+tk.E)
 
+def eq():
+    current = Label["text"]
+    
+    result: str = ""
+    try:
+        result = eval(current)
+    except Exception as e:
+        result = "ERROR! Check console."
+        print(f"error: {e}")
+
+    Label.configure(text=str(result))
+btn_eq = tk.Button(buttonframe, text="=", font=("Arial", 18), command=eq)
+btn_eq.grid(row=3, column=2, sticky=tk.W+tk.E)
+
+
 buttonframe.pack(fill='x', side=tk.BOTTOM)
+#end of the buttonframe
 
 root.mainloop()
-
-
-
-#don't mind it
-
-x: float = 9.0
-y: float = 1.0
-a: float = x
-b: float = y
-
-def add():
-    result = a + b
-    return result
-
-def sub():
-    result = a - b
-    return result
-
-def mul():
-    result = a * b
-    return result
-
-def div():
-    result = a / b
-    return result
